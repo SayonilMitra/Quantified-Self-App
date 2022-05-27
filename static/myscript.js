6a150e425ac92455694608babd11a395297a8206
Sayonil Mitra <sayonilmitra@gmail.com>
2022-05-27 10:50:14 +0530

    finalized look of homepage

---
+++ static/myscript.js
@@ -0,0 +1,30 @@
+function showPassword() {
+    let password = document.getElementById("password");
+    let password1 = document.getElementById("password1");
+    let password2 = document.getElementById("password2");
+
+    if (password.getAttribute("type") === "password") {
+        password.setAttribute('type', "text");
+    } else {
+        password.setAttribute('type', "password");;
+    }
+
+    if (password1.getAttribute("type") === "password") {
+        password1.setAttribute('type', "text");
+    } else {
+        password1.setAttribute('type', "password");;
+    }
+
+    if (password2.getAttribute("type") === "password") {
+        password2.setAttribute('type', "text");
+    } else {
+        password2.setAttribute('type', "password");;
+    }
+}
+
+function changeForm() {
+    let login_form_visibility = document.getElementById("login_form_main");
+    let register_form_visibility = document.getElementById("register_form_main");
+    login_form_visibility.classList.toggle("d-none");
+    register_form_visibility.classList.toggle("d-none");
+}
